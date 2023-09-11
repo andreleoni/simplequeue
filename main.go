1e455be2bf225b311c340b1e00b0d1871aee18fc
Andre Leoni <andreluizleoni@gmail.com>
2023-09-10 21:26:15 -0300

    change behaviour as tests

--- main.go
+++ main.go
@@ -12,24 +12,37 @@ import (
 func main() {
 	fmt.Println(simplequeue.Registers)
 
-	for _, worker := range simplequeue.Registers {
-		fmt.Println(worker.QueueName(), worker.PerformAsync("oi leoni"))
+	for i := 0; i < 100; i++ {
+		for _, worker := range simplequeue.Registers {
+			fmt.Println(worker.QueueName(), worker.PerformAsync("oi leoni"))
+		}
 	}
 
 	receivedQueueData := simplequeue.Receiver("default")
 
-	for _, queueData := range receivedQueueData {
-		messageData := simplequeue.MessageAttributes{}
+	for {
+		for _, queueData := range receivedQueueData {
+			fmt.Println("count", len(queueData))
+			messageData := simplequeue.MessageAttributes{}
 
-		err := json.Unmarshal([]byte(queueData), &messageData)
-		if err != nil {
-			log.Fatal(err)
+			err := json.Unmarshal([]byte(queueData), &messageData)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Println("attributes", messageData)
+
+			gotStruct := simplequeue.Registers[messageData.WorkerName]
+
+			gotStruct.Perform(messageData.Data)
 		}
 
-		fmt.Println("attributes", messageData)
+		receivedQueueData := simplequeue.Receiver("default")
 
-		gotStruct := simplequeue.Registers[messageData.WorkerName]
-
-		gotStruct.Perform(messageData.Data)
+		if len(receivedQueueData) < 10 {
+			break
+		}
 	}
+
+	fmt.Println("queues are empty")
 }
